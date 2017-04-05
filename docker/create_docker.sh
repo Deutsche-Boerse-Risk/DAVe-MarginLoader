35c7b3fe7ccbac372a93788f247bcff030cbb676
Michal Zerola <michal.zerola@gmail.com>
2017-04-05 09:55:11 +0200

    Docker deployment #31

---
+++ docker/create_docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Copy the DAVe binaries
+cp -r -v ./target/dave-margin-loader-1.0-SNAPSHOT/dave-margin-loader-1.0-SNAPSHOT ./docker/dave-margin-loader-1.0-SNAPSHOT
+
+# Delete the prefilled
+rm -r ./docker/dave-margin-loader-1.0-SNAPSHOT/etc/marginloader.conf
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker build -t dbgdave/dave-margin-loader:${CIRCLE_SHA1} ./docker/
+docker tag -f dbgdave/dave-margin-loader:${CIRCLE_SHA1} docker.io/dbgdave/dave-margin-loader:${CIRCLE_SHA1}
+docker push dbgdave/dave-margin-loader:${CIRCLE_SHA1}
+docker tag -f dbgdave/dave-margin-loader:${CIRCLE_SHA1} docker.io/dbgdave/dave-margin-loader:${CIRCLE_BRANCH}
+docker push dbgdave/dave-margin-loader:${CIRCLE_BRANCH}
