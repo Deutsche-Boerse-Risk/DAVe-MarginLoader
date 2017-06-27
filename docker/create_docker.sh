a6df0eef3642037ac82e22783647d48cd2beea26
Michal Zerola <michal.zerola@gmail.com>
2017-06-27 14:17:13 +0200

    Adjusting docker skript for new Docker version. #75

--- docker/create_docker.sh
+++ docker/create_docker.sh
@@ -12,9 +12,9 @@ sed -i 's/\(\s\+sslKey\s\+=\).*/\1\ \"\"/' ./docker/"${DAVE_CONFIG_FILE}"
 sed -i 's/\(\s\+sslCert\s\+=\).*/\1\ \"\"/' ./docker/"${DAVE_CONFIG_FILE}"
 sed -i 's/\(\s\+sslTrustCerts\s\+=\).*/\1\ \[\]/' ./docker/"${DAVE_CONFIG_FILE}"
 
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker build -t dbgdave/dave-margin-loader:${CIRCLE_SHA1} ./docker/
-docker tag -f dbgdave/dave-margin-loader:${CIRCLE_SHA1} docker.io/dbgdave/dave-margin-loader:${CIRCLE_SHA1}
+docker tag dbgdave/dave-margin-loader:${CIRCLE_SHA1} docker.io/dbgdave/dave-margin-loader:${CIRCLE_SHA1}
 docker push dbgdave/dave-margin-loader:${CIRCLE_SHA1}
-docker tag -f dbgdave/dave-margin-loader:${CIRCLE_SHA1} docker.io/dbgdave/dave-margin-loader:${CIRCLE_BRANCH}
+docker tag dbgdave/dave-margin-loader:${CIRCLE_SHA1} docker.io/dbgdave/dave-margin-loader:${CIRCLE_BRANCH}
 docker push dbgdave/dave-margin-loader:${CIRCLE_BRANCH}
